b6d3411621c6ab0679cba8fe946a13a26813452e
Amiraj Dhawan <amirajdhawan@gmail.com>
2014-10-30 14:04:19 +0530

    Added mail for student invite

---
+++ app/views/emails/eyic/student_invite.blade.php
@@ -0,0 +1,7 @@
+<p>Dear Student,<br/>
+<br/>
+You have been added as a student under the project {{$proj}} by your mentor {{$mentor}}.<br/>
+Please login at <a href="http://elsiportal.e-yantra.org">e-Yantra</a> with the following details:<br/>
+<br/>
+Username: {{$username}}<br/>
+Password: {{$pwd}}<br/>
