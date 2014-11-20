3ce7c0f926c8d6e1cf16f33cd1b0ca1d44cda41e
Viral Bhiwandiwala <viralb101@gmail.com>
2014-11-20 10:34:01 +0000

    login-navbar.blade.php edited online with Bitbucket

--- app/views/layouts/login-navbar.blade.php
+++ app/views/layouts/login-navbar.blade.php
@@ -15,7 +15,7 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right" style="font-size: 20px; margin-top: 20px;">
-					<li>eYantra Ideas Competition (eYIC) Pilot</li>
+					<li>eYantra Ideas Competition (eYIC-2015)</li>
 				</ul>
 			</div>      
 		</div>
