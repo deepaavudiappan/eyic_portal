929c6300d17bbe0615dd361c0c179bdf3a0f24e6
Viral Bhiwandiwala <viralb101@gmail.com>
2014-11-20 10:50:49 +0000

    addcoor.blade.php edited online with Bitbucket-changes stage 1,2,3

--- app/views/eyic/addcoor.blade.php
+++ app/views/eyic/addcoor.blade.php
@@ -18,7 +18,7 @@
 		@else
 		<div class="panel panel-info">
 			<div class="panel-heading">
-				<h3 class="panel-title">Register for eYantra Ideas Competition (eYIC) Pilot</h3>
+				<h3 class="panel-title">Register for eYantra Ideas Competition (eYIC-2015)</h3>
 			</div>
 			<div class="panel-body">
 				{{ Form::open(array('route' => 'addCoorSave', 'method' => 'POST')) }}
@@ -31,7 +31,7 @@
 					@if ($errors->has('pcoor_name')) <p class="help-block">{{ $errors->first('pcoor_name') }} </p>@endif
 				</div>
 				<div class="form-group @if ($errors->has('pcoor_email')) has-error @endif">
-					<label class="control-label" for="lbl_pcoor_email">Primary Coordinator Email:</label>
+					<label class="control-label" for="lbl_pcoor_email">Primary Coordinator e-mail:</label>
 					<input type="text" id="pcoor_email" class="form-control" name="pcoor_email" placeholder="example@example.com" value="{{ Input::old('pcoor_email') }}"/>
 					@if ($errors->has('pcoor_email')) <p class="help-block">{{ $errors->first('pcoor_email') }} </p>@endif
 				</div>
@@ -41,7 +41,7 @@
 					@if ($errors->has('scoor_name')) <p class="help-block">{{ $errors->first('scoor_name') }} </p>@endif
 				</div>
 				<div class="form-group @if ($errors->has('scoor_email')) has-error @endif">
-					<label for="lbl_scoor_email">Secondary Coordinator Email:</label>
+					<label for="lbl_scoor_email">Secondary Coordinator e-mail:</label>
 					<input type="text" id="scoor_email" class="form-control" name="scoor_email" placeholder="example@example.com" value="{{ Input::old('scoor_email') }}"/>
 					@if ($errors->has('scoor_email')) <p class="help-block">{{ $errors->first('scoor_email') }} </p>@endif
 				</div>
@@ -61,7 +61,7 @@
 				<h3 class="panel-title">1. What is eYIC?</h3>
 			</div>
 			<div class="panel-body">
-				<p>eYIC is e-Yantra Ideas competition, an initiative to encourage innovative projects from your robotics labs set up through the e-Yantra Lab Setup Initiative (eLSI), exclusively for the eLSI colleges.</p>
+				<p>eYIC stands for e-Yantra Ideas Competition, an initiative to encourage innovative projects from your robotics labs set up through the e-Yantra Lab Setup Initiative (eLSI),in colleges across the country.</p>
 			</div>
 		</div>
 
@@ -71,11 +71,11 @@
 			</div>
 			<div class="panel-body">
 				<ul>
-					<li>To enable students to attain benefit of the eLSI labs that have been setup in your college, e-Yantra has taken initiative called e-Yantra Ideas Competition (eYIC).</li>
-					<li>To encourage innovative ideas from students in eLSI colleges across the country.</li>
+					<li>To ensure sustained use of the robotics labs set up through the e-Yantra Lab Setup Initiative (eLSI).</li>
+					<li>To encourage<strong> innovative ideas</strong> from students in eLSI colleges across the country.</li>
 					<li>To provide a platform for talented teams to showcase their projects.</li>
 					<li>To nurture BE projects in embedded systems and robotics at the eLSI colleges.</li>
-					<li>To ensure sustained use of the robotics labs set up through the e-Yantra Lab Setup Initiative (eLSI).</li>
+					
 				</ul>
 			</div>
 		</div>
@@ -87,9 +87,21 @@
 			<div class="panel-body">
 				<p>e-Yantra Ideas Competition (eYIC- 2015) will be held as part of e-Yantra Symposium (eYS-2015).</p>
 				<strong>Stage 1:</strong><br/>
-				<p>It would require the teams to submit a proposal – an idea -- for a project. The qualified teams from Stage 1 would be then required to implement the same project and submit a working demonstration of the proposed idea through a video link.</p>
+				<ul>
+				<li> Team submit a proposal – an idea -- for a project as per a given template.</li>
+				<li> Proposals are evaluated and teams are selected for Stage-2.</li>
+				</ul>
 				<strong>Stage 2:</strong><br/>
-				<p>Teams selected after Stage 2 will get a chance to showcase their projects in e-Yantra Symposium (eYS-2015) at IIT-B. These projects will be judged and exciting prizes and certificates will be awarded to the winning ideas!</p><br/>
+				<ul>
+				<li>Selected Teams implements their proposed idea.</li>
+				<li>A video of the working demonstration of the idea is submitted through a video link.</li>
+				<li>Selected teams will be invited to showcase their idea in an exhibition during the e-YS-2015.</li>
+				<li>All Selected teams will get honorarium.</li>
+				</ul>
+				<strong>Stage 3:</strong><br/>
+				<ul>
+				<li>Exhibited Projects will be judged by a panel of judges.</li>
+				<li>Winning entries are awarded exciting Prizes and Certificates.</li>
 				<p>Projected Date for eYS-2015: Second week of April 2015.</p>
 			</div>
 		</div>
@@ -101,9 +113,9 @@
 			<div class="panel-body">
 				<ul>
 					<li>Since this would be a pilot phase we would be restricting the number of teams selected to a small number. A maximum of four teams per eLSI college is allowed to participate. College lab in-charge will select four teams from their college who will be registered for the competition.</li>
-					<li>Teams would consist of 3 or 4 student members mentored by one teacher. All the students should be from the same year but can be from different branch.</li>
+					<li>Teams would consist of 3 or 4 student members mentored by one teacher. All the students should be from the same year but can be from different branches.</li>
 					<li>The project must be from the domain of embedded systems and robotics.</li>
-					<li>Teams are required to use any processing board available in the eLSI labs. (Firebird V, Raspberry Pi, BeagleBoard & Arduino,etc)</li>
+					<li>Teams are required to use any processing board available in the eLSI labs. (Firebird V, Raspberry Pi, BeagleBoard & Arduino, etc.)</li>
 				</ul>
 			</div>
 		</div>
