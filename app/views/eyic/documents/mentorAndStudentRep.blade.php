bc004987f9cf4648f78e02c917956991e1352752
Amiraj Dhawan <amirajdhawan@gmail.com>
2014-12-11 13:19:04 +0530

    Added static pages in navbar

--- app/views/eyic/documents/mentorAndStudentRep.blade.php
+++ app/views/eyic/documents/mentorAndStudentRep.blade.php
@@ -32,26 +32,40 @@
 			</div>
 		</div>
 
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<h3 class="panel-title">Mentor’s Role:</h3>
+			</div>
+			<div class="panel-body">
 		
-		<h3>Mentor’s Role:</h3>
-		<ul>
-			<li>Teams comprising of 3 or 4 students and one teacher designated a <b>Mentor</b> submit their project proposals to the <b>Coordinator</b> (A local faculty designated by the college) by an internal deadline defined by the Coordinator.</li>
-			<ul>
-				<li>All the students from a team should be from the same year but can be from different branches.</li>
-				<li>One mentor can guide many projects but one student can be part of one project only.</li>
-			</ul>
-			<li>Out of all the submitted project proposals, coordinator selects up to four projects from the field of <b>embedded systems and robotics. These projects have to be implemented using the components from the Embedded systems and Robotics lab set up in the college through the e-Yantra Lab Setup Initiative (eLSI).</b></li>
-			<li>Coordinator will select the best four project ideas from the proposals received from the teams and register the Mentors. Note that the Coordinator can nominate up to 4 Mentors (for the 4 selected projects) by registering their e-mail ids.</li>
-			<li>Each nominated mentor will get a confirmation email of successful registration.</li>
-			<li>Mentor will fill details of the project in brief and nominate one of the students from the team as <b>Student Representative</b> to coordinate with e-Yantra.</li>
-		</ul>
-		<h3>Student Representative’s Role:</h3>
-		<ul>
-			<li>A student representative of a particular team is nominated by the mentor.</li>
-			<li>Student representative will then fill the details of each team member.</li>
-			<li>He/She will be co-ordinating between e-Yantra and the team.</li>
-			<li>All competition details and future communications regarding the competition will be mailed to the student representative and the Mentor.</li>
-		</ul>				
+				<ul>
+					<li>Teams comprising of 3 or 4 students and one teacher designated a <b>Mentor</b> submit their project proposals to the <b>Coordinator</b> (A local faculty designated by the college) by an internal deadline defined by the Coordinator.</li>
+					<ul>
+						<li>All the students from a team should be from the same year but can be from different branches.</li>
+						<li>One mentor can guide many projects but one student can be part of one project only.</li>
+					</ul>
+					<li>Out of all the submitted project proposals, coordinator selects up to four projects from the field of <b>embedded systems and robotics. These projects have to be implemented using the components from the Embedded systems and Robotics lab set up in the college through the e-Yantra Lab Setup Initiative (eLSI).</b></li>
+					<li>Coordinator will select the best four project ideas from the proposals received from the teams and register the Mentors. Note that the Coordinator can nominate up to 4 Mentors (for the 4 selected projects) by registering their e-mail ids.</li>
+					<li>Each nominated mentor will get a confirmation email of successful registration.</li>
+					<li>Mentor will fill details of the project in brief and nominate one of the students from the team as <b>Student Representative</b> to coordinate with e-Yantra.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="panel panel-info">
+			<div class="panel-heading">
+				<h3 class="panel-title">Student Representative’s Role:</h3>
+			</div>
+			<div class="panel-body">
+	
+				<ul>
+					<li>A student representative of a particular team is nominated by the mentor.</li>
+					<li>Student representative will then fill the details of each team member.</li>
+					<li>He/She will be co-ordinating between e-Yantra and the team.</li>
+					<li>All competition details and future communications regarding the competition will be mailed to the student representative and the Mentor.</li>
+				</ul>
+			</div>
+		</div>				
 	</div>
 </div>
 
