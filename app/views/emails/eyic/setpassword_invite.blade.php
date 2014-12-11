5ced4e1e3ca0baa12b022dd03b003f547a3df3ed
Amiraj Dhawan <amirajdhawan@gmail.com>
2014-12-11 13:55:05 +0530

    Added static pages in navbar

--- app/views/emails/eyic/setpassword_invite.blade.php
+++ app/views/emails/eyic/setpassword_invite.blade.php
@@ -1,7 +1,7 @@
 <p>Dear User,<br/>
 <br/>
-Please find below the link to reset your password for your Account : {{$username}} on <a href="http://elsiportal.e-yantra.org">eYIC</a>.<br/>
-<a href="http://elsiportal.e-yantra.org/validateToken/{{$username}}/{{$token}}"></a><br/>
+Please find below the link to reset your password for your Account : {{$username}} for eLSI portal.<br/>
+<a href="http://elsiportal.e-yantra.org/validateToken/{{$username}}/{{$token}}">Reset your password!</a><br/>
 In case you have any queries, Please contact us at helpdesk@e-yantra.org via email about the issue.
 <br/>
 Best Wishes,<br/>
