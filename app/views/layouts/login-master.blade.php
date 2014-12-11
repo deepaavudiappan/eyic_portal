7b95556ec1a2443a85f4a379775aa4565cfe2098
Amiraj Dhawan <amirajdhawan@gmail.com>
2014-12-11 13:50:32 +0530

    Added static pages in navbar

--- app/views/layouts/login-master.blade.php
+++ app/views/layouts/login-master.blade.php
@@ -5,29 +5,29 @@
 	@show
 	<style>
 		.subMenu {
-		    background: linear-gradient(to right, #ffffff 0%, #f4f4f4 100%) repeat scroll 0 0 rgba(0, 0, 0, 0);
-		    border-right: 1px solid #e7e7e7;		    
-		    list-style: none outside none;
-		    padding-top: 55px;
+			background: linear-gradient(to right, #ffffff 0%, #f4f4f4 100%) repeat scroll 0 0 rgba(0, 0, 0, 0);
+			border-right: 1px solid #e7e7e7;		    
+			list-style: none outside none;
+			padding-top: 55px;
 		}
 		.subMenu li {		    
-		    border-top : 1px solid #e7e7e7;
-		    border-bottom : 1px solid #e7e7e7;
+			border-top : 1px solid #e7e7e7;
+			border-bottom : 1px solid #e7e7e7;
 		}		
 		.subMenu a {
-		    border-right: 1px solid #e7e7e7;
-		    color: #333333;
-		    display: block;
-		    margin-right: -1px;
-		    padding: 14px 0;
+			border-right: 1px solid #e7e7e7;
+			color: #333333;
+			display: block;
+			margin-right: -1px;
+			padding: 14px 0;
 		}
 		.subMenu a:hover, .subMenu .active a {
-		    background: no-repeat scroll left top #ffffff;
-		    border-right: medium none;
-		    color: #66bb00;
-		    text-decoration: none;
+			background: no-repeat scroll left top #ffffff;
+			border-right: medium none;
+			color: #66bb00;
+			text-decoration: none;
 		}		
-</style>
+	</style>
 	<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 </head>
 <body>
@@ -35,18 +35,20 @@
 	<!-- Container -->
 	<div class="container" style="padding-top: 80px;">
 		<!-- Content -->
-		@if (!empty($success))
-			<div class="alert alert-success">{{$success}}</div>
+		@if(Session::has('success'))
+		<div class="alert alert-success">{{ Session::get('success') }}</div>
+		@elseif (!empty($success))
+		<div class="alert alert-success">{{ $success }}</div>
 		@endif
-	
+		
 		@if ($errors->has())
-			<div class="alert alert-danger text-center">	
+		<div class="alert alert-danger text-center">	
 			@foreach ($errors->all() as $error)
-				{{ $error }}<br/>
+			{{ $error }}<br/>
 			@endforeach
-			</div>
+		</div>
 		@endif
-	
+		
 		@yield('content')
 	</div>
 
