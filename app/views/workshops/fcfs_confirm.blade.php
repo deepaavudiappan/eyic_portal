c2cc7f7900d33581459918f11971b71ed54ae149
Amiraj Dhawan <amirajdhawan@gmail.com>
2015-01-14 15:01:00 +0530

    Workshop reg

--- app/views/workshops/fcfs_confirm.blade.php
+++ app/views/workshops/fcfs_confirm.blade.php
@@ -6,7 +6,7 @@
 @if (!(Session::has('success')) )
 <div class="panel panel-info">
 	<div class="panel-heading">
-		<h3 class="panel-title">Workshop Confirmation</h3>
+		<h3 class="panel-title">Workshop Registration</h3>
 	</div>
 	<div class="panel-body">
 		<div class="row text-justify">
