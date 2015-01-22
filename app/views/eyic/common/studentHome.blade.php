f09274a3921d037e505eac7c1fba347797559d65
Amiraj Dhawan <amirajdhawan@gmail.com>
2015-01-22 09:21:14 +0530

    Added propject proposal upload

--- app/views/eyic/common/studentHome.blade.php
+++ app/views/eyic/common/studentHome.blade.php
@@ -2,52 +2,55 @@
 @section('styles')
 @stop
 
-@section('content')	
-	<table class="table table-striped">
-	  <tr>
-	  	<th colspan="2">
-	  		<h3><span class="glyphicon glyphicon-user" aria-hidden="true"></span> {{ $studentDetail->name }}</h3>
-	  	</th>
-	  </tr>
-	  <tr>
+@section('content')
+<div class="alert alert-danger">
+	Project Proposal can now be uploaded by the Student Representative by clicking on the "Project Proposal" tab on the left side bar.
+</div>
+<table class="table table-striped">
+	<tr>
+		<th colspan="2">
+			<h3><span class="glyphicon glyphicon-user" aria-hidden="true"></span> {{ $studentDetail->name }}</h3>
+		</th>
+	</tr>
+	<tr>
 		<td>COLLEGE NAME</td>
 		<td><span class="glyphicon glyphicon-home" aria-hidden="true"></span> {{ $studentDetail->college }}</td>		
-	  </tr>
-	  <tr>
+	</tr>
+	<tr>
 		<td>E-MAIL ID</td>
 		<td><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> {{ $studentDetail->email_id }}</td>		
-	  </tr>
-	  <tr>
+	</tr>
+	<tr>
 		<td>CONTACT</td>
 		<td><span class="glyphicon glyphicon-phone-alt" aria-hidden="true"></span> {{ $studentDetail->contact_num }}</td>		
-	  </tr>
-	  <tr>
+	</tr>
+	<tr>
 		<td>Address</td>
 		<td><span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span> {{ $studentDetail->address }}</td>		
-	  </tr>	 
-	</table>	
-	<div class="row">
-		<div class="col-md-12 text-center">
-			{{ HTML::linkRoute('changePwdLand', 'Change Password', [], ['class'	=> 'btn btn-primary']) }}
-		</div>
+	</tr>	 
+</table>	
+<div class="row">
+	<div class="col-md-12 text-center">
+		{{ HTML::linkRoute('changePwdLand', 'Change Password', [], ['class'	=> 'btn btn-primary']) }}
 	</div>
+</div>
 @stop
 
 @section('notice')
-		
-		<div class="panel panel-default">
-		  <div class="panel-heading">Notice 1</div>
-		  <div class="panel-body">
-		    	Please put notice 1 here
-		  </div>
-		</div>
 
-		<div class="panel panel-default">
-		  <div class="panel-heading">Notice 2</div>
-		  <div class="panel-body">
-		    	Please put notice 2 here
-		  </div>
-		</div>
+<div class="panel panel-default">
+	<div class="panel-heading">Notice 1</div>
+	<div class="panel-body">
+		Please put notice 1 here
+	</div>
+</div>
+
+<div class="panel panel-default">
+	<div class="panel-heading">Notice 2</div>
+	<div class="panel-body">
+		Please put notice 2 here
+	</div>
+</div>
 @stop
 
 @section('scripts')
