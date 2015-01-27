d396352cb64ea846758984e8d6c9bda8bf4f8edb
Amiraj Dhawan <amirajdhawan@gmail.com>
2015-01-27 11:53:35 +0530

    Workshop

--- app/views/workshops/fcfs_confirm_final.blade.php
+++ app/views/workshops/fcfs_confirm_final.blade.php
@@ -22,14 +22,40 @@
 				<br/>
 				To encourage more colleges from your region to participate in this Nation-building initiative, we welcome one team of 4 teachers from your college to participate in the workshop. Selection for participation is on a First Come First Serve (FCFS) basis to fill the capacity of 15 college teams.<br/>
 				<br/>
+				@if($clgs->region == 'Bangalore')
 				Date: <strong>February 6th and 7th, 2015</strong><br/>
 				Venue: <strong>IIIT Bangalore<br/>
 				<span style="margin-left:50px;">No. 26/C, Electronic City, Hosur Road</span><br/>
 				<span style="margin-left:50px;">Bangalore - 560100</span><br/></strong>
 				Coordinator: <strong>Ms. Roshni DSouza</strong><br/>
 				Contact number: <strong>+91-80-4140-7777, +91-9986668421</strong><br/>
-				e-mail: <strong>roshni.dsouza@iiitb.ac.in</strong><br/>
+				E-mail: <strong>roshni.dsouza@iiitb.ac.in</strong><br/>
 				<br/>
+				Please confirm attendance of your team of four teachers on or before <strong>January 26th 2015</strong>:<br/>
+				<br/>
+				@elseif ($clgs->region == 'Bhopal')
+				Date: <strong>February 13th and 14th, 2015</strong><br/>
+				Venue: <strong>NRI Institute of Information Science & Technology,<br/>
+				<span style="margin-left:50px;">1, Sajjan Singh Nagar, Opposite Patel Nagar,</span><br/>
+				<span style="margin-left:50px;">Raisen Road, Bhopal, Madhya Pradesh 462023</span><br/></strong>
+				Coordinator: <strong>Dr. Amita Mahor</strong><br/>
+				Contact number: <strong>0755-2684058, +91-94250-19572</strong><br/>
+				E-mail: <strong>mitu2008mahor@gmail.com</strong><br/>
+				<br/>
+				Please confirm attendance of your team of four teachers on or before <strong>February 3rd 2015</strong>:<br/>
+				<br/>
+				@elseif ($clgs->region == 'Gujarat')
+				Date: <strong></strong><br/>
+				Venue: <strong>-<br/>
+				<span style="margin-left:50px;">--</span><br/>
+				<span style="margin-left:50px;">--</span><br/></strong>
+				Coordinator: <strong>--</strong><br/>
+				Contact number: <strong>--</strong><br/>
+				E-mail: <strong>--</strong><br/>
+				<br/>
+				Please confirm attendance of your team of four teachers on or before <strong>February -- 2015</strong>:<br/>
+				<br/>
+				@endif
 				<div class="alert alert-danger">
 					<strong>Please note:<br/></strong>
 					<br/>
