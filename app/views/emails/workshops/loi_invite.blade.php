ec0b76d54c36670f6c5fa70e71aa799ccba93292
Amiraj Dhawan <amirajdhawan@gmail.com>
2015-02-04 14:52:47 +0530

    emails update

--- app/views/emails/workshops/loi_invite.blade.php
+++ app/views/emails/workshops/loi_invite.blade.php
@@ -31,9 +31,11 @@
 					<tr><td><strong>e-mail: </strong></td><td>{{$email;}}</td></tr><br/>
 				</table>
 				<br/>
+				<div style="text-align:center;">
 				For workshop schedule click here: <a href="http://elsiportal.e-yantra.org/elsi/workshop/college/schedule">Schedule!</a><br/><br/>
 				<a href="http://elsiportal.e-yantra.org/elsi/workshop/college/confirm_land_loi?ct={{ $token }}" style="background-color: #337ab7;border-color: #2e6da4;font-size: 14px;font-weight: 400;line-height: 1.42857143;text-align: center;color:white;text-decoration:none;border: 1px solid transparent;border-radius: 4px;color: #fff;margin-top: 5px;margin-bottom: 5px;padding: 6px 12px;">Click here to confirm your participation</a><br/>
 				<br/>
+				</div>
 				Here are the modalities of the workshop:<br/>
 				<br/>
 				<hr/>
